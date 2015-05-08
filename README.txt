6ac18a77eb3582df646f15840146d22bfc9e26de
Fried Hoeben <bitbucket@hsac.nl>
2015-05-08 21:57:43 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,23 @@
+This projects allows the use of HSAC's FitNesse fixtures (https://github.com/fhoeben/hsac-fitnesse-fixtures) and Xebia's Xebium (http://xebia.github.io/Xebium/) together in a single FitNesse installation. Both fixture classes will connect to the same browser instance using Selenium.
+
+It is mostly targeted to allow organisation's having made an investment in tests using Xebium to migrate to HSAC's fixtures, which we believe will allow them to have a more understandable and maintainable test set.
+
+The setup for this bridge is as follows:
+Create a Selenium webdriver is set up using HSAC's SeleniumDriverSetup fixture (in a SuiteSetUp page) and
+use nl.hsac.fitnesse.fixture.slim.web.xebium.HsacSeleniumDriverFixture instead of com.xebia.incubator.xebium.SeleniumDriverFixture (which will also (like BrowserTest) use the Selenium browser created in the SuiteSetUp).
+
+Features:
+- Allows a build server run to control the Selenium setup as per usual for HSAC's fixtures (see https://github.com/fhoeben/hsac-fitnesse-fixtures/wiki/3.1.-Build-Server-Selenium-Configuration).
+- Screenshots taken using Xebium's standard screenshot feature are stored in 'files/screenshots/xebium' which is included in FitNesse's results for a run by a build server (i.e. target/fitnesse-results).
+- HsacSeleniumDriverFixture also supports taking of screenshots (so can be used in 'storyboard' tables, and supports |show|take screenshot|myPage|).
+- On any exception thrown by HsacSeleniumDriverFixture an attempt is made to include an screenshot.
+- When 'isOn' returns 'Execution of command failed' this is transformed to an exception containing a screenshot.
+- 'doOn' and 'doOnWith' with a target based on id or xPath now first wait (for at most the specified timeout) until that target is visible, and will throw an exception when it is not.
+- 'click' and 'clickAndWait' selecting using xPath will use BrowserTest's click, which ensures the element is visible (possibly first scrolling it into view).
+- 'waitForVisible' (with an xPath target) throws an exception containing screenshot if the element is not visible after the maximum timeout.
+
+
+Please note:
+When 'startBrowserOnUrl' is invoked no new Selenium WebDriver session is created, but it opens the specified URL, after deleting the existing localStorage and cookies for that site.
+'stopBrowser' does not end the WebDriver session (that must be done, like starting it, via nl.hsac.fitnesse.fixture.slim.web.SeleniumDriverSetup), it only clears the cookies and localStorage of the current site.
+To use have screenshots be included with every exception it is important to configure the 'Slim Interaction' used (by adding '!define slim.flags {-i nl.hsac.fitnesse.slim.interaction.InterceptingInteraction}' to the root page of your Slim tests).
---
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/web/xebium/HsacSeleniumDriverFixture.java
@@ -0,0 +1,152 @@
+package nl.hsac.fitnesse.fixture.slim.web.xebium;
+
+import fitnesse.slim.fixtureInteraction.FixtureInteraction;
+import nl.hsac.fitnesse.fixture.Environment;
+import nl.hsac.fitnesse.fixture.slim.SlimFixtureException;
+import nl.hsac.fitnesse.slim.interaction.ExceptionHelper;
+import nl.hsac.fitnesse.slim.interaction.InteractionAwareFixture;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * Our subclass of Xebium to allow combination with HSAC fixtures.
+ */
+public class HsacSeleniumDriverFixture extends com.xebia.incubator.xebium.SeleniumDriverFixture
+        implements InteractionAwareFixture {
+    private final Environment environment = Environment.getInstance();
+    private final HsacXebiumBrowserTest browserTest;
+
+    public HsacSeleniumDriverFixture() {
+        this(new HsacXebiumBrowserTest());
+    }
+
+    public HsacSeleniumDriverFixture(HsacXebiumBrowserTest browserTest) {
+        this.browserTest = browserTest;
+        setTimeoutToSeconds(environment.getSeleniumHelper().getDefaultTimeoutSeconds());
+        try {
+            String baseDir = environment.getFitNesseRootDir() + "/files/screenshots/xebium/";
+            saveScreenshotAfterInFolder("assertion", baseDir.replace("/", File.separator));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void startBrowserOnUrl(String browser, String browserUrl) {
+        // we use the driver configured in SuiteSetUp
+        WebDriver driver = environment.getSeleniumHelper().driver();
+        this.startDriverOnUrl(driver, browserUrl);
+        cleanBrowserOnUrl(browserUrl);
+    }
+
+    @Deprecated
+    @Override
+    public void startBrowserOnUrlUsingRemoteServerOnHostOnPort(final String browser, final String browserUrl, final String serverHost, final int serverPort) {
+        startBrowserOnUrl(browser, browserUrl);
+    }
+
+    protected void cleanBrowserOnUrl(String browserUrl) {
+        HsacXebiumBrowserTest browserTest = this.browserTest;
+        browserTest.open(browserUrl);
+        cleanBrowser();
+        browserTest.refresh();
+    }
+
+    protected void cleanBrowser() {
+        HsacXebiumBrowserTest browserTest = this.browserTest;
+        browserTest.deleteAllCookies();
+        browserTest.clearLocalStorage();
+    }
+
+    @Override
+    public void stopBrowser() {
+        cleanBrowser();
+        // stopping browser is done in SuiteTearDown
+    }
+
+    @Override
+    public void setTimeoutTo(long timeout) {
+        getBrowserTest().secondsBeforeTimeout((int) (timeout / 1000));
+        super.setTimeoutTo(timeout);
+    }
+
+    /*
+     * To support 'storyboard' tables.
+     * @param baseName base name for screenshot
+     * @return html image
+     */
+    public String takeScreenshot(String baseName) {
+        return getBrowserTest().takeScreenshot(baseName);
+    }
+
+    @Override
+    public boolean doOn(String command, String target) {
+        boolean result;
+        if ("waitForVisible".equals(command) && !target.startsWith("css=")) {
+            result = getBrowserTest().waitForXPathVisible(target);
+        } else if ("click".equals(command) && !target.startsWith("css=")) {
+            result = getBrowserTest().clickByXPath(target);
+        } else if ("clickAndWait".equals(command) && !target.startsWith("css=")) {
+            result = getBrowserTest().clickByXPath(target);
+        } else {
+            if (!command.contains("Not")) {
+                ensureTargetVisible(target);
+            }
+            result = super.doOn(command, target);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean doOnWith(String command, String target, String value) {
+        if (!command.contains("Not")) {
+            ensureTargetVisible(target);
+        }
+        return super.doOnWith(command, target, value);
+    }
+
+    protected void ensureTargetVisible(String target) {
+        // we can not act on elements that are not visible
+        if (target.startsWith("/")) {
+            getBrowserTest().waitForXPathVisible(target);
+        } else if (target.startsWith("id=")) {
+            getBrowserTest().waitForVisible(By.id(target.substring(3)));
+        }
+    }
+
+    @Override
+    public String isOn(String command, String target) {
+        String result = super.isOn(command, target);
+        if (result != null && result.startsWith("Execution of command failed:")) {
+            try {
+                String screenshotHtml = takeScreenshot("isOn");
+                String msg = String.format("<div>%s. Page content:%s</div>", result, screenshotHtml);
+                throw new SlimFixtureException(false, msg);
+            } catch (SlimFixtureException e) {
+                throw e;
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+
+        return result;
+    }
+
+    public HsacXebiumBrowserTest getBrowserTest() {
+        return browserTest;
+    }
+
+    @Override
+    public Object aroundSlimInvoke(FixtureInteraction interaction, Method method, Object... arguments) {
+        try {
+            return interaction.methodInvoke(method, this, arguments);
+        } catch (Throwable t) {
+            Throwable realEx = ExceptionHelper.stripReflectionException(t);
+            throw getBrowserTest().asSlimFixtureException(realEx);
+        }
+    }
+}
