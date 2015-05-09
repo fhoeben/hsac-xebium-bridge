c7905177f4db92809c6d0da96c1a9113e7b1b3f1
Fried Hoeben <github@hsac.nl>
2015-05-09 10:23:50 +0200

    Comments to document why methods are there

--- src/main/java/nl/hsac/fitnesse/fixture/slim/web/xebium/HsacXebiumBrowserTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/web/xebium/HsacXebiumBrowserTest.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.By;
  * BrowserTest with some extra methods to assist HsacSeleniumDriverFixture.
  */
 public class HsacXebiumBrowserTest extends BrowserTest {
+    // override to make publicly visible
     @Override
     public boolean waitForVisible(final By by) {
         return super.waitForVisible(by);
     }
 
+    // override to make publicly visible
     public SlimFixtureException asSlimFixtureException(Throwable t) {
         return  (SlimFixtureException) handleException(null, null, t);
     }
