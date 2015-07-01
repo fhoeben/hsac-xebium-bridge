017b2d163a6cefe2f1d463d38db39d3fb22dfdf5
Fried Hoeben <github@hsac.nl>
2015-07-01 20:45:58 +0200

    Do not rely on @WaitUntil when calling BrowserTest methods directly from Java, and not from the wiki (#9)

--- src/main/java/nl/hsac/fitnesse/fixture/slim/web/xebium/HsacXebiumBrowserTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/web/xebium/HsacXebiumBrowserTest.java
@@ -2,13 +2,40 @@ package nl.hsac.fitnesse.fixture.slim.web.xebium;
 
 import nl.hsac.fitnesse.fixture.slim.SlimFixtureException;
 import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 /**
  * BrowserTest with some extra methods to assist HsacSeleniumDriverFixture.
  */
 public class HsacXebiumBrowserTest extends BrowserTest {
-    // override to make publicly visible
     public SlimFixtureException asSlimFixtureException(Throwable t) {
         return  (SlimFixtureException) handleException(null, null, t);
+    }
+
+    @Override
+    public boolean click(final String place) {
+        // since this method is invoked by HsacSeleniumDriverFixture, and not just Slim
+        // using the WaitUntl annotion does not suffice. A call which will always be done
+        // is needed.
+        return waitUntil(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return HsacXebiumBrowserTest.super.click(place);
+            }
+        });
+    }
+
+    @Override
+    public boolean waitForVisible(final String place) {
+        // since this method is invoked by HsacSeleniumDriverFixture, and not just Slim
+        // using the WaitUntl annotion does not suffice. A call which will always be done
+        // is needed.
+        return waitUntilOrStop(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return HsacXebiumBrowserTest.super.waitForVisible(place);
+            }
+        });
     }
 }
